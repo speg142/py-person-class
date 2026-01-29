a4a9a29299080a8885b910b760c80f4231f258a9
unknown <vanyatop23012010@gmail.com>
2026-01-29 18:04:40 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> str:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    for person_dict in people:
+        Person(person_dict["name"], person_dict["age"])
+    for person_dict in people:
+        person_obj = Person.people[person_dict["name"]]  # Находим объект
+        if "wife" in person_dict and person_dict["wife"] is not None:
+            person_obj.wife = Person.people[person_dict["wife"]]
+        if "husband" in person_dict and person_dict["husband"] is not None:
+            person_obj.husband = Person.people[person_dict["husband"]]
+    return [Person.people[person_dict["name"]] for person_dict in people]
